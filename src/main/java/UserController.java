f88a8c5a21326e5b0747ecc103e9abd59c2e2303
mindaugasciulada <m.ciulada@gmail.com>
2019-04-09 19:00:50 +0300

    Second task delete imported

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,36 +1,16 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
-import com.google.gson.JsonParser;
-
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.protocol.HTTP;
-
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONException;
-import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-
-import javax.ws.rs.core.MediaType;
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 
