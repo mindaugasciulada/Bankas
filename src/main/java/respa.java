de4d009d3a68104e7d97278776ed49e1620c6715
mindaugasciulada <m.ciulada@gmail.com>
2019-04-10 09:32:03 +0300

    FIX

---
+++ src/main/java/respa.java
@@ -0,0 +1,26 @@
+public class respa{
+    private int id;
+    private String name;
+    private String active;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    //        public void setExample(String example) {
+//            this.example = example;
+//        }
+    public String getActive() {
+        return active;
+    }
+    public int getPrice() {
+        return price;
+    }
+//        public void setFr(String fr) {
+//            this.fr = fr;
+//        }
+}
